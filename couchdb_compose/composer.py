32d4291b59e09afd1a4e3c77bd60f4604c185ca1
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-06-01 18:09:41 +0200

    first part of the example works

---
+++ couchdb_compose/composer.py
@@ -0,0 +1,22 @@
+import py
+import yaml
+import copy
+
+class Composer(object):
+    def __init__(self, path):
+
+        self.path = path
+        with path.join('couchdb-compose.yml').open() as fp:
+            self.config = yaml.load(fp)
+        self.doc = copy.deepcopy(self.config['doc'])
+
+    def push(self, attrs, data):
+        d = self.doc
+        for attr in attrs[:-1]:
+            d = d.setdefault(attr, {})
+        d[attrs[-1]] = data #XXX: conflicts
+
+from .ddoc import load_objects
+actions = [load_objects]
+
+
