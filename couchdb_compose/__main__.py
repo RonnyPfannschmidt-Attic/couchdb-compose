61c0c2b1470c614b5affc62393f1f17ed874a04c
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-06-02 17:27:36 +0200

    push command

--- couchdb_compose/__main__.py
+++ couchdb_compose/__main__.py
@@ -11,21 +11,31 @@ subparsers = parser.add_subparsers()
 
 
 
-def show(composer):
+def show(args, composer):
     json.dump(composer.doc.keys(), py.std.sys.stdout, indent=2)
+
 
 
 show_parser = subparsers.add_parser('show')
 show_parser.set_defaults(func=show)
 
 
+def push(args, composer):
+    import couchdbkit
+    server = couchdbkit.Server()
+    db = server.get_or_create_db(args.database)
+    print('storing to', db)
+    db.save_doc(composer.doc, force=True)
 
+push_parser = subparsers.add_parser('push')
+push_parser.set_defaults(func=push)
+push_parser.add_argument('database')
 
 def main():
     args = parser.parse_args()
     composer = Composer(py.path.local(args.path))
     composer.run_actions(actions)
-    args.func(composer)
+    args.func(args, composer)
     
 
 main()
