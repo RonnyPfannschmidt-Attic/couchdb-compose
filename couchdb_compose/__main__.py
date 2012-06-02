b8df4143fc81a80be873fdd3541c6503a28c6721
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-06-02 16:38:20 +0200

    first acceptance test, restructure

--- couchdb_compose/__main__.py
+++ couchdb_compose/__main__.py
@@ -12,9 +12,7 @@ parser.add_argument('path', nargs='?', default=py.std.os.getcwd())
 def main():
     args = parser.parse_args()
     composer = Composer(py.path.local(args.path))
-    for action in actions:
-        print('start', action.__name__, file=py.std.sys.stderr)
-        action(composer)
+    composer.run_actions(actions)
     json.dump(composer.doc.keys(), py.std.sys.stdout, indent=2)
 
 main()
