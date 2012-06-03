60e5511f09d56f107a140e5b9128ab9222850365
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-06-03 11:54:26 +0200

    functional tests for the cli calls

--- couchdb_compose/__main__.py
+++ couchdb_compose/__main__.py
@@ -3,17 +3,17 @@ import py
 import json
 from couchdb_compose.composer import Composer, actions
 
+
 from argparse import ArgumentParser
 parser = ArgumentParser()
+
 
 parser.add_argument('--path', nargs='?', default=py.std.os.getcwd())
 subparsers = parser.add_subparsers()
 
 
-
 def show(args, composer):
     json.dump(composer.doc.keys(), py.std.sys.stdout, indent=2)
-
 
 
 show_parser = subparsers.add_parser('show')
@@ -27,15 +27,18 @@ def push(args, composer):
     print('storing to', db)
     db.save_doc(composer.doc, force=True)
 
+
 push_parser = subparsers.add_parser('push')
 push_parser.set_defaults(func=push)
 push_parser.add_argument('database')
+
 
 def main():
     args = parser.parse_args()
     composer = Composer(py.path.local(args.path))
     composer.run_actions(actions)
     args.func(args, composer)
-    
 
-main()
+
+if __name__ == '__main__':
+    main()
