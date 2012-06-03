ff61b2beb75e8efd5e3c63671c4458759a40bd68
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-06-03 14:08:56 +0200

    add sensible deploy_view functions

--- couchdb_compose/__main__.py
+++ couchdb_compose/__main__.py
@@ -21,6 +21,9 @@ show_parser.set_defaults(func=show)
 
 
 def push(args, composer):
+    if args.deploy_views:
+        deploy_views(args, composer)
+    
     import couchdbkit
     server = couchdbkit.Server()
     db = server.get_or_create_db(args.database)
@@ -28,10 +31,32 @@ def push(args, composer):
     db.save_doc(composer.doc, force=True)
 
 
-push_parser = subparsers.add_parser('push')
+push_parser = subparsers.add_parser('push', help='stores the composed doc to the db')
 push_parser.set_defaults(func=push)
 push_parser.add_argument('database')
+push_parser.add_argument('--deploy-views', action='store_true',
+                         help='deploy views before pushing',
+                        )
 
+
+def deploy_views(args, composer):
+
+    server = couchdbkit.Server()
+    db = server.get_or_create_db(args.database)
+    print('storing to', db)
+    newid = composer.doc['_id'] + ":view-deploy"
+    newdoc = dict(composer.doc, _id=newid)
+    view_basename = newid.split('/', 1)[1] + '/'
+
+    
+    db.save_doc(newdoc, force=True)
+
+    for view in newdoc['views']:
+        list(db.view(view_basename+view, limit=1))
+
+
+deploy_views_parser = subparsers.add_parser('deploy_views', help='stores the ddoc to a different id and updates all views, usefull for view updates before a push')
+deploy_views_parser.set_defaults(func=deploy_views)
 
 def main():
     args = parser.parse_args()
