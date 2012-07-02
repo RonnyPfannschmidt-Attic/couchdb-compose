7f2f2d76dfe4475692023eb4d07e0ad3b2777bc6
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-07-02 16:24:19 +0200

    initial externals support

--- couchdb_compose/externals.py
+++ couchdb_compose/externals.py
@@ -1,5 +1,44 @@
+import restkit
+
+README="""\
+This Directory is used to cache couchdb composer externals,
+it can be deleted to refresh the cache
+"""
+
+def make_cache(path):
+    cache = path.ensure('.couchdb-compose-external-cache', dir=1)
+    cache.join('README.txt').write(README)
+
+    return cache
+
+
+def download_external(external, path):
+    url = external['url']
+
+    resp = restkit.request(url)
+    path.join(url.split('/')[-1]).write(resp.body_string())
+    path.ensure('.completed')
+
+
+def maybe_download_external(external, path):
+    if not path.join('.completed').check():
+        download_external(external, path)
+
+
+def add_external(composer, external, path):
+    pass
 
 
 def include_externals(composer):
     externals = composer.getlist('externals')
+    if not externals:
+        return
+
+    cache = make_cache(composer.path)
+
+
+    for external in externals:
+        path = cache.ensure(external['name'], dir=1)
+        maybe_download_external(external, cache)
+        add_external(composer, external, path)
 
