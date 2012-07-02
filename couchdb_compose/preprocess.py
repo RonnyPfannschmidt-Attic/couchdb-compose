1a9f45335a47a47a814465e83f6804736678fa6d
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-07-02 19:19:57 +0200

    add nonverbose show, better coffescript2js attachments

--- couchdb_compose/preprocess.py
+++ couchdb_compose/preprocess.py
@@ -4,16 +4,27 @@ from execjs import ProgramError
 
 DOT_COFFEE = '.coffee'
 
+def compile_coffeescript(data):
+    try:
+        data = coffeescript.compile(data, bare=True)
+    except ProgramError as exc:
+        print repr(data)
+        raise SyntaxError(exc.args[0])
+    return data.strip()
+
 
 def commonjs_to_coffeescript(path, name, data):
     if isinstance(data, str) and name.endswith(DOT_COFFEE):
-        try:
-            data = coffeescript.compile(data, bare=True)
-        except ProgramError as exc:
-            print repr(data)
-            raise SyntaxError(exc.args[0])
-        return name[:-len(DOT_COFFEE)], data.strip()
+        return name[:-len(DOT_COFFEE)], compile_coffeescript(data)
 
+
+def attachment_to_coffeescript(path, name, info):
+    if path==('_attachments',) and name.endswith(DOT_COFFEE):
+        info = dict(info,
+            data=compile_coffeescript(info['data']),
+            content_type="application/javascript",
+        )
+        return name.replace(DOT_COFFEE, '.js') , info
 
 
 def preprocess(data, path, processors):
@@ -34,7 +45,10 @@ def preprocess(data, path, processors):
                 preprocess(v, path + (k,), processors)
 
 
-preprocessors = [commonjs_to_coffeescript]
+preprocessors = [
+    commonjs_to_coffeescript,
+    attachment_to_coffeescript,
+]
 
 def run_preprocessors(composer):
     preprocess(composer.doc, (), preprocessors)
