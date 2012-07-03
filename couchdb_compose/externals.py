6f66e205a3048c6f563367e532a4aabb6ace8d8e
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-07-03 11:14:40 +0200

    add external zip unpack so i can use blueprint

--- couchdb_compose/externals.py
+++ couchdb_compose/externals.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+import io
 import restkit
+import zipfile
 
 README="""\
 This Directory is used to cache couchdb composer externals,
@@ -14,9 +17,11 @@ def make_cache(path):
 
 def download_external(external, path):
     url = external['url']
+    print(' * download', url)
 
-    resp = restkit.request(url)
-    path.join(url.split('/')[-1]).write(resp.body_string())
+    resp = restkit.request(url, follow_redirect=True)
+
+    path.join(url.split('/')[-1]).write(resp.body_string(), 'wb')
     path.ensure('.completed')
 
 
@@ -28,8 +33,30 @@ def maybe_download_external(external, path):
 def add_external(composer, external, path):
     filename = external['url'].split('/')[-1]
     attachment = external.get('attachment')
+    unpack = external.get('unpack')
     content = path.join(filename).read()
-    if attachment:
+    
+    if unpack:
+        fp = io.BytesIO(content)
+        zipfp = zipfile.ZipFile(fp)
+        names = zipfp.namelist()
+        for item in unpack:
+            sourcename = item['from']
+            #XXX hack for zipfile prefix
+            for name in names:
+                if name.endswith('/' + sourcename):
+                    content = zipfp.read(name)
+                    break
+            else:
+                print(' !', sourcename, 'not found')
+            info = composer.add_attachment(item['to'], content)
+            info.update(
+                url = external.get('url'),
+                name = external.get('name'),
+            )
+
+
+    elif attachment:
         info = composer.add_attachment(attachment, content)
         info.update(
             url = external.get('url'),
